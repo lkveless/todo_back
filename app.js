046fef16002f1bb2398888d54d75c3bfa574e25c
lkveless <ard01@mail.ru>
2021-11-10 14:09:00 +0300

    rework app js and add new funcs

--- app.js
+++ app.js
@@ -1,19 +1,30 @@
 const express = require('express')
-
 const app = express()
-
+const apiAuthRouter = require('./controllers/auth.controller')
+const apiTodoRouter = require('./controllers/todo.controller')
+const apiUserRouter = require('./controllers/user.controller')
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+const { notFound, errorHandler, asyncHandler } = require('./middleware/middleware')
+const { initDB } = require('./models/todo.index.js')
 
-//Подключение к базе данных через sequelize
-const db = require('./models/todo.index');
-db.sequelize.sync()
+initDB()
 
-//Функция, отвечающая за сброс всех таблиц и значений базы данных, для первого запуска программы небходимо раскомментировать, при последующих - закомментировать.
-//db.sequelize.sync({ force: true }).then(() => {
-//  console.log("Drop and re-sync db.");
-//});
 
+
+app.use((req, res, next) => {
+  console.log('URL = ', req.url)
+  console.log('Original_URL = ', req.originalUrl)
+  console.log('METHOD = ', req.method)
+  console.log('HOST = ', req.headers.host)
+  console.log('IsSecure = ', req.secure)
+  console.log('BODY', req.body)
+  console.log('QUERY', req.query)
+
+  next()
+});
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 8080
 app.listen(PORT, () => { console.log(`server is listening on ${PORT}`) })
@@ -21,5 +32,6 @@ app.listen(PORT, () => { console.log(`server is listening on ${PORT}`) })
 app.get('/', (req, res) => {
   res.json("welcome to the todo application")
 })
-require('./routes/todos.routes')(app)
-
+app.use('/api/todos', apiTodoRouter)
+app.use('/api/auth', apiAuthRouter)
+app.use('/api/user', apiUserRouter)
